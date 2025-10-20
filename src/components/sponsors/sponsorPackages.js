f5dd2a665e834d7f513704c5f5ab9ee92a1a14eb
Stephen Parker <sparker888@gmail.com>
2025-10-20 16:41:58 -0400

    added early bird to pricing

--- src/components/sponsors/sponsorPackages.js
+++ src/components/sponsors/sponsorPackages.js
@@ -276,7 +276,7 @@ export default function SponsorPackages() {
                 <br />
                 InfoZone Sponsorship
               </h3>
-              <p className="mt-2 text-xl font-semibold text-wri-dark-blue">$7,000.00</p>
+              <p className="mt-2 text-xl font-semibold text-wri-dark-blue">$7,000.00 Early Bird</p>
             </div>
             <div className="col-span-2">
               <p className="mb-4">
